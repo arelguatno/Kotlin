b32f72440a8060af63a8bdce2ca4c8a3e9cfd23b
Arel Guatno <arelguatno@yahoo.com>
2022-09-10 15:42:51 +0100

    rate

--- Budget Budy/app/src/main/java/com/example/budgetbuddy/screens/settings_screen/SettingsFragment.kt
+++ Budget Budy/app/src/main/java/com/example/budgetbuddy/screens/settings_screen/SettingsFragment.kt
@@ -1,6 +1,9 @@
 package com.example.budgetbuddy.screens.settings_screen
 
 import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -216,5 +219,18 @@ class SettingsFragment : MainFragment() {
         binding.settingsDisplay.themes.setOnClickListener {
             it.findNavController().navigate(R.id.action_settingsFragment_to_themesFragment)
         }
+
+        binding.settingsAbout.rate.setOnClickListener {
+            openNewTabWindow(getString(R.string.view_on_google_play),requireContext())
+        }
+    }
+
+    private fun openNewTabWindow(urls: String, context: Context) {
+        val uris = Uri.parse(urls)
+        val intents = Intent(Intent.ACTION_VIEW, uris)
+        val b = Bundle()
+        b.putBoolean("new_window", true)
+        intents.putExtras(b)
+        context.startActivity(intents)
     }
 }
