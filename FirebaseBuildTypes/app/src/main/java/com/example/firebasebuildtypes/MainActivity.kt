1efc84530fea3fdeb21fd64d1043ca378ea64951
Arel Guatno <arelguatno@yahoo.com>
2022-09-19 17:21:35 +0100

    firebase flavors

---
+++ FirebaseBuildTypes/app/src/main/java/com/example/firebasebuildtypes/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.example.firebasebuildtypes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+    fun saveMe(view: View) {
+        val db = Firebase.firestore
+
+        val user = hashMapOf(
+            "first" to "Budget Buddy",
+            "last" to "Budget Buddy",
+            "born" to 1815
+        )
+
+        db.collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+            }
+            .addOnFailureListener { e ->
+                Log.w(TAG, "Error adding document", e)
+            }
+    }
+}
